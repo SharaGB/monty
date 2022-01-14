fb2cd8f544d41fcb13a56889d2639271cb16fda7
SharaGB <shara.garcia.betancur@gmail.com>
2022-01-13 23:44:26 -0500

    Add functions aux for opcodes

--- fun_aux.c
+++ fun_aux.c
@@ -6,54 +6,114 @@
  */
 int is_digit(char *digit)
 {
-	int i = 0;
+	int i;
 
-	if (digit[i] == '-')
+	for (i = 0; digit[i]; i++)
 	{
-		i++;
-	}
-	while (digit[i])
-	{
-		if (digit[i] >= '0' && digit[i] <= '9')
+		if (digit[i] == '-' && i == 0)
 		{
-			return (0);
+			continue;
 		}
 		if (isdigit(digit[i]) == 0)
 		{
 			return (1);
 		}
-		i++;
 	}
-	return (1);
+	return (0);
+}
+
+/**
+ * add_node - Add a new node.
+ * @stack: Header of the list(stack)
+ * @n: Value new node
+ * Return: pointer to the new node, or NULL on failure
+ */
+stack_t *add_node(stack_t **stack, const int n)
+{
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	if (!stack)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	if (!*stack)
+	{
+		new_node->prev = new_node;
+		new_node->next = new_node;
+	}
+	else
+	{
+		(*stack)->prev->next = new_node;
+		new_node->prev = (*stack)->prev;
+		(*stack)->prev = new_node;
+		new_node->next = *stack;
+	}
+	if (global.data == STACK || global.size == 0)
+	{
+		*stack = new_node;
+	}
+	return (new_node);
 }
 
 /**
  * free_stack - Frees the stack
- * @head: The stack
+ * @fre: Exit check
+ * @args: Line arguments
  */
-void free_stack(stack_t **head)
+void free_stack(int fre, void *args)
 {
-	stack_t *tmp;
+	stack_t **stack;
+	stack_t *next;
 
-	while (*head != NULL)
+	(void)fre;
+
+	stack = (stack_t **)args;
+	if (*stack)
 	{
-		tmp = (*head);
-		*head = tmp->prev;
-		free(tmp);
+		(*stack)->prev->next = NULL;
+		(*stack)->prev = NULL;
+	}
+	while (*stack != NULL)
+	{
+		next = (*stack)->next;
+		free(*stack);
+		*stack = next;
+	}
+	global.size = 0;
+}
+
+/**
+ * frees - Frees the array
+ * @fre: Exit check
+ * @args: Line arguments
+ */
+void frees(int fre, void *args)
+{
+	char **ptr = args;
+
+	(void)fre;
+	if (*ptr != NULL)
+	{
+		free(*ptr);
 	}
 }
 
 /**
- * error_handler - Function that error handler.
- * @err_m: Error message
- * @line_err: Line data
+ * fp_close - Close file
+ * @status: status passed to exit
+ * @args: Line arguments
  */
-void error_handler(char *err_m, unsigned int line_err)
+void fp_close(int status, void *arg)
 {
-	if (line_err)
-	{
-		fprintf(stderr, "L%d: ", line_err);
-	}
-	fprintf(stderr, "%s\n", err_m);
-	exit(EXIT_FAILURE);
+	FILE *fp;
+
+	(void)status;
+
+	fp = (FILE *) arg;
+	fclose(fp);
 }
